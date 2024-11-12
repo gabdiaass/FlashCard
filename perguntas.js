5b765a1bbf077d636c4a05a10d63dcf3381e4bdd
gabdiaass <143522316+gabdiaass@users.noreply.github.com>
2024-11-12 11:56:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'mangás',
+    'O que é um mangá?',
+    'São histórias em quadrinhos japonesas. Ele possui características marcantes que o difere das demais revistas em quadrinhos.'
+)
+
+criaCartao(
+    'Bruninho',
+    'Quem é Bruno Mars?',
+    'Peter Gene Hernandez,mais conhecido pelo nome artístico Bruno Mars,é um cantor, compositor, produtor musical, dançarino e multi-instrumentista.    '
+)
+
+criaCartao(
+    'Programação',
+    'melhor música da Taylor Swift?',
+    'Sua melhor música é All Too Well, segundo fãs.'
+)
+
+criaCartao(
+    'anime',
+    'Qual melhor anime da atualidade?',
+    'egundo votações one piece é o melhor da atualidade'
+)
